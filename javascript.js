eb46633b70069f540f5ed0ceada2e01f61176384
Franek Sornat <62433571+Kaptvr@users.noreply.github.com>
2023-08-25 10:03:42 +0200

    Refactor registerButton function

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ function animateButton () {
             e.target.classList.add('pressed')
         })
     })
-//////////////////////// change button classes to 'operator' and 'operand'
+    
     buttons.forEach(button => {
         button.addEventListener('mouseup', e => {
             e.target.classList.remove('pressed')
@@ -26,11 +26,17 @@ function registerButton () {
     const buttons = document.querySelectorAll('button');
     buttons.forEach(button => {
         button.addEventListener('mouseup', e => {
-            if (e.target.textContent === 'AC') {
-                clearEquation();
-            } else {
+            if (e.target.classList[0] == 'operand' ||
+                e.target.classList[0] == 'operator' ||
+                e.target.classList[0] == 'comma') {
                 equation += e.target.textContent;
                 updateEquationField ();
+            } else if (e.target.classList[0] == 'AC') {
+                clearEquation();
+            } else if (e.target.classList[0] == 'Delete') {
+                deleteLastChar();
+            } else if (e.target.classList[0] == 'equal') {
+                evaluateEquasion();
             }
         })
     })
@@ -43,79 +49,22 @@ function updateEquationField () {
 
 function clearEquation () {
     equation = ''
-    console.log(equation)
-    const equationField = document.querySelector('.equation');
-    equationField.textContent = '';
+    updateEquationField();
+}
+
+function deleteLastChar () {
+    equation = equation.slice(0, equation.length - 1);
+    updateEquationField();
+}
+
+function evaluateEquasion () {
+
 }
 
 
 function main() {
     registerButton();
-    updateEquationField();
     animateButton();
 }
 
 main ();
-
-// function animateButton () {
-//     const buttons = document.querySelectorAll('button');
-
-//     buttons.forEach(button => {
-//         button.addEventListener('mousedown', e => {
-//             e.target.classList.add('pressed')
-//         })
-//     })
-
-//     buttons.forEach(button => {
-//         button.addEventListener('mouseup', e => {
-//             e.target.classList.remove('pressed')
-//         })
-//     })
-
-//     buttons.forEach(button => {
-//         button.addEventListener('mouseout', e => {
-//             e.target.classList.remove('pressed')
-//         })
-//     })
-// }
-
-// function registerButton (equation) {
-//     const buttons = document.querySelectorAll('button');
-//     buttons.forEach(button => {
-//         button.addEventListener('mouseup', e => {
-//             console.log(e.target.textContent)
-//             if (e.target.textContent === 'AC') {
-//                 clearEquation(equation);
-//             } else {
-//                 equation += e.target.textContent;
-//                 updateEquationField (equation);
-//             }
-//         })
-//     })
-// }
-
-// function updateEquationField (equation) {
-//     const equationField = document.querySelector('.equation');
-//     equationField.textContent = equation;
-// }
-
-// function clearEquation (equation) {
-//     equation = ''
-//     console.log(equation)
-//     const equationField = document.querySelector('.equation');
-//     equationField.textContent = '';
-// }
-
-
-// function main() {
-//     // let equation = '';
-
-//     registerButton (equation);
-//     updateEquationField (equation);
-//     animateButton();
-
-    
-
-// }
-
-// main ();
